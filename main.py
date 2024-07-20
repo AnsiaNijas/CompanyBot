6746b202bfa0efd8c99050ceb17ec32d3b448492
Ansia Nijas <35911053+AnsiaNijas@users.noreply.github.com>
2024-07-20 02:19:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from Database import load_documents, split_documents, add_to_chroma, check_clear_database
+from chatbot import chatbot_chat
+from retrival import query_rag
 
 app = FastAPI()
 CHROMA_PATH="chroma_data"
@@ -10,3 +12,8 @@ async def create_upload_file(file: UploadFile):
     chunks=split_documents(documents)
     add_to_chroma(chunks)
     return {"Loaded":{len(documents)},"chunks":chunks,"filename": file.filename}
+
+@app.get("/")
+def read_root():
+    formatted_response=chatbot_chat()
+    return {"Response": formatted_response}
