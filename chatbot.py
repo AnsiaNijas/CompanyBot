77f9bf0c8a9986c4c68967044658803b3d07063b
Ansia Nijas <35911053+AnsiaNijas@users.noreply.github.com>
2024-07-31 15:12:29 +0100

    Add files via upload
     Imported necessary modules from langchain, openai, and other utils.
    - Defined PROMPT_TEMPLATE for creating response context.
    - Implemented chatbot_chat function to handle user queries.
    - Integrated query retrieval using query_rag function.
    - Formatted the prompt and sent it to OpenAI's ChatCompletion API.
    - Processed and formatted the response along with source metadata.

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,16 @@
 from langchain_community.vectorstores import Chroma
-from get_embedding_function import get_embedding_function
+import openai
+from models import get_embedding_function
 import argparse
 from langchain.prompts import ChatPromptTemplate
+from Database import query_rag
+import os 
+from dotenv import load_dotenv
 
-CHROMA_PATH="chroma_data"
+load_dotenv()
+
+openai.api_key = os.getenv("API_KEY")
+
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
 
@@ -13,23 +20,27 @@ Answer the question based only on the following context:
 
 Answer the question based on the above context: {question}
 """
-def query_rag(query_text: str):
-    # Prepare the DB.
-    embedding_function = get_embedding_function()
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-    # Search the DB.
-    results = db.similarity_search_with_score(query_text, k=5)
+def chatbot_chat(query_text,history):
+   
+    results=query_rag(query_text)
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    # print(prompt)
+    #print(prompt)
+    response = openai.ChatCompletion.create(
+        model="gpt-4o",
+        messages=[  {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
+    ]
+ )
 
-    model = Ollama(model="mistral")
-    response_text = model.invoke(prompt)
-
+    response_text = response.choices[0].message["content"]
     sources = [doc.metadata.get("id", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
-    print(formatted_response)
-    return response_text
+    return formatted_response
+
+
+   
+
