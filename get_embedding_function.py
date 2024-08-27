d6be9fd6ba0a16a580892ca16809f92c24735195
Ansia Nijas <35911053+AnsiaNijas@users.noreply.github.com>
2024-08-27 16:53:24 +0100

    Delete get_embedding_function.py
    Consolidate models by adding function to models.py
    
    Moved the function from get_embedding_function.py to models.py to centralize all model definitions in a single file for improved organization and maintainability.

--- get_embedding_function.py
+++
@@ -1,8 +0,0 @@
-from chromadb.utils import embedding_functions
-from langchain_community.embeddings import SentenceTransformerEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
-
-def get_embedding_function():
-    embeddings = SentenceTransformerEmbeddings(model_name="nomic-ai/nomic-embed-text-v1", model_kwargs={"trust_remote_code":True})
-    
-    return embeddings
